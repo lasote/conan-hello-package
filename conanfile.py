cacaa8c4209b21af327cda2a904335bba4e736fe
Luis Martinez de Bartolome <lasote@gmail.com>
2018-10-16 18:17:58 +0200

    Fixing hello world package

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,24 @@
 from conans import ConanFile, CMake
-   
+
+
 class HelloConan(ConanFile):
     name = "Hello"
     version = "0.1"
     license="MIT"
     settings = "os", "compiler", "build_type", "arch"
     url = "https://github.com/memsharded/conan-hello.git"
-
-    def source(self):
-        self.run("git clone https://github.com/memsharded/hello.git")
+    exports_sources = "*"
+    generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
-        self.run('cd hello && cmake . %s' % cmake.command_line)
-        self.run("cd hello && cmake --build . %s" % cmake.build_config)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*.lib", dst="lib", src="hello/lib")
-        self.copy("*.a", dst="lib", src="hello/lib")
+        self.copy("*.h", dst="include", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib",  keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["hello"]
